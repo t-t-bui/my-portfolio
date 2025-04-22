b863d5912e4dde7e0dc330edb5d85d9fec815e81
t-t-bui <tbui931958@gmail.com>
2025-04-22 13:32:58 -0500

    Updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import '../styles/Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import '../styles/footer.css';
 
 export default function Footer() {
   return (
